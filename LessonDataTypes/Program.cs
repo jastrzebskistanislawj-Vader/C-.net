38c3a04e81ec07ed73849c95df331da0c277088d
Stanislaw.J <jastrzebskistanislaw.j@gmail.com>
2026-03-16 13:00:48 +0100

    dalsza nauka

--- LessonDataTypes/Program.cs
+++ LessonDataTypes/Program.cs
@@ -1,66 +1,16 @@
-﻿int val_A = 2;
-int val_B = val_A;
-val_B = 5;
+﻿int value1 = 11;
+decimal value2 = 6.2m;
+float value3 = 4.3f;
 
-Console.WriteLine("--Value Types--");
-Console.WriteLine($"val_A: {val_A}");
-Console.WriteLine($"val_B: {val_B}");
+// Your code here to set result1
+// Hint: You need to round the result to nearest integer (don't just truncate)
+int result1 = (int)Math.Round(value1 / (double)value2);
+Console.WriteLine($"Divide value1 by value2, display the result as an int: {result1}");
 
+// Your code here to set result2
+decimal result2 = value2 / (decimal)value3;
+Console.WriteLine($"Divide value2 by value3, display the result as a decimal: {result2}");
 
-
-int[] ref_A= new int[1];
-ref_A[0] = 2;
-int[] ref_B = ref_A;
-ref_B[0] = 5;
-
-Console.WriteLine("--Reference Types--");
-Console.WriteLine($"ref_A[0]: {ref_A[0]}");
-Console.WriteLine($"ref_B[0]: {ref_B[0]}");
-
-
-
-
-
-//rzutowanie typów danych
-// int first = 2;
-// string second = "4";
-// string result = first + second;
-// Console.WriteLine(result);
-
-
-// int myInt = 3;
-// Console.WriteLine($"int: {myInt}");
-
-// decimal myDecimal = myInt;
-// Console.WriteLine($"decimal: {myDecimal}");
-
-
-// decimal myDecimal = 3.14m;
-// Console.WriteLine($"decimal: {myDecimal}");
-
-// int myInt = (int)myDecimal;
-// Console.WriteLine($"int: {myInt}");
-
-
-decimal myDecimal = 1.23456789m;
-float myFloat = (float)myDecimal;
-
-Console.WriteLine($"Decimal: {myDecimal}");
-Console.WriteLine($"Float  : {myFloat}");
-
-
-int first = 5;
-int second = 7;
-string message = first.ToString() + second.ToString();
-Console.WriteLine(message);
-
-string value = "102";
-int result = 0;
-if (int.TryParse(value, out result))
-{
-   Console.WriteLine($"Measurement: {result}");
-}
-else
-{
-   Console.WriteLine("Unable to report the measurement.");
-}
+// Your code here to set result3
+float result3 = value3 / value1;
+Console.WriteLine($"Divide value3 by value1, display the result as a float: {result3}");
